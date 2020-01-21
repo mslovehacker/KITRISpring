69bc54b778bc64a408de9948e86b1d32781da40b
mslovehacker <mslovehacker@gmail.com>
2020-01-21 16:01:32 +0900

    Source add

---
+++ SpringGit/src/kr/co/mskim/cont/MainCont.java
@@ -0,0 +1,11 @@
+package kr.co.mskim.cont;
+
+public class MainCont {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Spring");
+
+	}
+
+}
